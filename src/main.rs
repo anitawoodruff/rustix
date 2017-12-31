b25476dfc10dc0df88f39cc11c748553bd3d9b7a
Anita Woodruff <ilovejam@gmail.com>
2017-12-31 12:04:06 +0000

    everything (in the middle of a wrapping error)

---
+++ src/main.rs
@@ -0,0 +1,163 @@
+use std::collections::HashMap;
+
+// This is for implementing to_string.
+use std::fmt::{
+    Display,
+    Formatter,
+    Result as FmtResult,
+};
+
+struct Cube {
+    solved : bool,
+}
+
+impl Display for Cube {
+    fn fmt(&self, formatter: &mut Formatter) -> FmtResult {
+        const COLORS: &[&str] = &["y", "r", "w", "p"];
+        fn get_color(solved: bool, face_index: usize) -> &'static str{
+            let unwrapped_color_index =
+                if solved {face_index} else {face_index - 1};
+            let wrapped_color_index = COLORS.len() + unwrapped_color_index % COLORS.len();
+            COLORS[wrapped_color_index]
+        }
+        let top = get_color(self.solved, 0);
+        let front = if self.solved {COLORS[1]} else {COLORS[0]};
+        let back_front = if self.solved {COLORS[2]} else {COLORS[1]};
+        let back_bottom = if self.solved {COLORS[3]} else {COLORS[2]};
+        write!(formatter, "
+                ____________             ______1_____
+               /  y  /  {}  /|           /|     |     |
+              /_____/_____/ |          /g|  w  |  {}  |
+             /  y  /  {}  /|b|         /| |_____|_____|2
+            /_____/_____/ | |        |g|/|     |     |
+            |     |     |b|/|        | /g|  w  |  {}  |
+            |  r  |  {}  | /b|        |/| |_____|_____|
+            |_____|_____|/| |        |g|/  p  /  {}  /
+            |     |     |b|/         | /_____/_____/
+            |  r  |  {}  | /2         |/  p  /  {}  /
+            |_____|_____|/           |_____/_____/
+                  1
+", top, back_front, top, back_front, front, back_bottom, front, back_bottom)
+    }
+}
+
+impl Cube {
+    fn is_solved(&self) -> bool {
+        return self.solved;
+    }
+
+    fn twist(&mut self) {
+        self.solved = !self.solved;
+    }
+
+    fn twist_back(&mut self) {
+        self.solved = !self.solved;
+    }
+}
+
+fn build_cube() -> Cube {
+    Cube {
+        solved : true,
+    }
+}
+
+fn print_cube(cube: &Cube) {
+    println!("{}", cube);
+}
+
+fn print_solved_status(cube: &Cube) {
+    println!("{}", if cube.is_solved() {" SOLVED ! "} else { " Not solved yet..."});
+}
+
+fn print_cube_and_solved_status(cube: &Cube) {
+    print_cube(&cube);
+    print_solved_status(&cube);
+}
+
+fn main() {
+    println!("Hello, world!");
+
+    let mut cube = build_cube();
+    print_cube_and_solved_status(&cube);
+
+    cube.twist();
+    print_cube_and_solved_status(&cube);
+
+    cube.twist_back();
+    print_cube_and_solved_status(&cube);
+}
+
+#[cfg(test)]
+mod test {
+
+    use super::build_cube;
+
+    #[test]
+    fn test_new_cube_is_solved() {
+        let cube = build_cube();
+        assert!(cube.is_solved());
+    }
+
+    #[test]
+    fn test_twisted_cube_is_not_solved() {
+        let mut cube = build_cube();
+        cube.twist();
+        assert!(!cube.is_solved());
+    }
+
+    #[test]
+    fn test_twist_and_twist_back_is_solved() {
+        let mut cube = build_cube();
+        cube.twist();
+        assert!(!cube.is_solved());
+        cube.twist_back();
+        assert!(cube.is_solved());
+    }
+
+    #[test]
+    fn test_cube_to_string() {
+        let cube = build_cube();
+        let expected = "
+                ____________             ______1_____
+               /  y  /  y  /|           /|     |     |
+              /_____/_____/ |          /g|  w  |  w  |
+             /  y  /  y  /|b|         /| |_____|_____|2
+            /_____/_____/ | |        |g|/|     |     |
+            |     |     |b|/|        | /g|  w  |  w  |
+            |  r  |  r  | /b|        |/| |_____|_____|
+            |_____|_____|/| |        |g|/  p  /  p  /
+            |     |     |b|/         | /_____/_____/
+            |  r  |  r  | /2         |/  p  /  p  /
+            |_____|_____|/           |_____/_____/
+                  1
+";
+        assert!(
+            cube.to_string() == expected,
+            "Expected {} but got {}", expected, cube);
+    }
+
+    #[test]
+    fn test_twisted_cube_to_string() {
+        let mut cube = build_cube();
+
+        cube.twist();
+
+        let expected = "
+                ____________             ______1_____
+               /  y  /  p  /|           /|     |     |
+              /_____/_____/ |          /g|  w  |  r  |
+             /  y  /  p  /|b|         /| |_____|_____|2
+            /_____/_____/ | |        |g|/|     |     |
+            |     |     |b|/|        | /g|  w  |  r  |
+            |  r  |  y  | /b|        |/| |_____|_____|
+            |_____|_____|/| |        |g|/  p  /  w  /
+            |     |     |b|/         | /_____/_____/
+            |  r  |  y  | /2         |/  p  /  w  /
+            |_____|_____|/           |_____/_____/
+                  1
+";
+        assert!(
+            cube.to_string() == expected,
+            "Expected {} but got {}", expected, cube);
+    }
+}
